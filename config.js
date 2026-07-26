0f2b0ec2ee33b1189500d9b58d5103af5b8e2b0a
NMGMASTER35 <nmorris210509@gmail.com>
2026-07-26 23:10:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // IMPORTANT: Any key placed in a static website can be viewed by visitors.
 // Leave blank to use unauthenticated requests, or use a serverless proxy to hide a key.
 window.APP_CONFIG = {
-  tflApiKey: ""
+  tflApiKey: "1234567890abcdef1234567890abcdef"
 };
